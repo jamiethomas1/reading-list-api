7721929ebd6b2edf8946a3455247b964f6797c6d
cogsincogs <jamiewthomas13@gmail.com>
2024-03-26 18:19:11 +0000

    Modify URI slightly

--- app/routes/api.php
+++ app/routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::group(['middleware' => ['api']], function() {
-    Route::get('/books/{id}', [BookController::class, 'getBooks']);
-    Route::post('/books/{id}', [BookController::class, 'newBook']);
+    Route::get('/books/{id?}', [BookController::class, 'getBooks']);
+    Route::post('/books/', [BookController::class, 'newBook']);
     Route::patch('/books/{id}', [BookController::class, 'editBook']);
     Route::delete('/books/{id}', [BookController::class, 'deleteBook']);
 });
