c6231bafc4b2bf18133cd6118d72d65bfb74447a
cogsincogs <jamiewthomas13@gmail.com>
2024-03-26 13:00:47 +0000

    Add initial books table migration

---
+++ app/database/migrations/2024_03_26_105015_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title"); // Throw error in API response if null
+            $table->json("authors_array")->default(new Expression('(JSON_ARRAY())'));
+            $table->date("published")->nullable(); // Check in API logic if null, return empty date string
+            $table->json("genres_array")->default(new Expression('(JSON_ARRAY())'));
+            $table->smallInteger("length_pages"); // Throw error in API response if null
+            $table->boolean("complete")->default(false); // When set to true, also set finished_reading to current timestamp
+            $table->smallInteger("current_page")->default(0);
+            $table->string("current_chapter")->default("");
+            $table->date("started_reading")->nullable(); // Check in API logic if null, return empty date string
+            $table->date("finished_reading")->nullable(); // Check in API logic if null, return empty date string
+            $table->string("cover_image_url")->nullable(); // Return null, make clear in docs that developer should provide default image
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
