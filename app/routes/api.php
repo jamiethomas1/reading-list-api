519bec4df701d0324acc03a8945fa8b7b4f67079
cogsincogs <jamiewthomas13@gmail.com>
2024-03-26 15:42:12 +0000

    Add routes to api.php

---
+++ app/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::group(['middleware' => ['api']], function() {
+    Route::get('/books/{id}', [BookController::class, 'getBooks']);
+    Route::post('/books/{id}', [BookController::class, 'newBook']);
+    Route::patch('/books/{id}', [BookController::class, 'editBook']);
+    Route::delete('/books/{id}', [BookController::class, 'deleteBook']);
+});
